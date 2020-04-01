12c9e7a490d8c2f2a18abc45e6086a160e7d1150
luizmaciel <luizmaciel@denox.com.br>
2020-04-01 11:58:22 -0300

    finalizacao

--- src/Pages/Eu/index.js
+++ src/Pages/Eu/index.js
@@ -1,21 +1,35 @@
 import React from 'react';
 
 import Header from '../Header';
-import { EuWrapper, ImagemEu, Blob } from './style';
-import euImg from '../../Assets/eu.png';
-import blob from '../../Assets/blob.svg';
+
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { EuWrapper, ImagemEu, Blob, LinkFotografia, DivIcones } from './style';
+import euImg from '../../Assets/eu.jpg';
+import blob from '../../Assets/bolha.png';
 
 export default function Eu(){
     return(
         <>
             <Header />
             <EuWrapper>
-                
+                <div style={{textAlign: "center", position: "relative"}}>
                     <ImagemEu src={euImg}/>
                     <Blob src={blob}/>
-            
-            
-                <p>blablabla</p>
+                </div>
+                <div>
+                    <h1>Luiz Pedro Maciel</h1>
+                    <p>
+                        Graduando em Ciência da Computação pela PUC, mora em Belo Horizonte - MG e possui 20 anos.
+                        Com Atualmente trabalha como desenvolvedor front-end e ja trabalhou com desenvolvimento SharePoint.
+                        Tem a <LinkFotografia href="https://www.instagram.com/uauluiz/" target="_blank">fotografia</LinkFotografia> como um hobby e gosta de equilibrar o final de semana com futebol, estudos e cerveja.
+                    </p>
+                    <DivIcones>
+                        <a target="_blank" href="https://github.com/Luiz-Pedro"><FaGithub size={36}/></a>
+                        <a target="_blank" href="https://www.linkedin.com/in/luizpedromaciel/"><FaLinkedin size={36}/></a>
+                        <p>lpedro.maciel@gmail.com</p>
+                    </DivIcones>
+                    
+                </div>
             </EuWrapper>
         </>
     );
--- src/Pages/Eu/style.js
+++ src/Pages/Eu/style.js
@@ -1,33 +1,98 @@
 import styled from 'styled-components';
 
 export const EuWrapper = styled.section`
-    display: flex;
-    align-items:center;
-    justify-content: space-around;
-    padding: 3em 0em 0em 0em;
-    position: relative;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 3em;
+    max-width: 80%;
+    margin: 0em auto;
+    margin-top: 6em;
+    height: auto;
+    align-items: center;
     @media (max-width: 1300px){
-        padding: 2em 3em 0em;
+        margin-top: 3em;
     }
     @media (max-width: 600px){
-        flex-direction: column;
-        padding: 4em 3em 0em;
+        grid-template-columns: 1fr;
+        margin-top: 3em;
+        margin-bottom: 2em;
+        grid-gap: 5em;
     }
 
-
+    h1{
+        color: #f7f8f7;
+        font-size: 2.5em;
+        margin-bottom: 0.5em;
+        @media (max-width: 1300px){
+            font-size: 2.3em;
+        }
+        @media (max-width: 600px){
+            font-size: 1.8em;
+        }
+    }
+    p{
+        color: #323232;
+        line-height: 26px;
+        letter-spacing: 1.02px;
+        padding-right: 4em;
+        @media (max-width: 1300px){
+            padding-right: 0em;
+        }
+        @media (max-width: 600px){
+            font-size: 0.9em;
+            padding-right: 0em;
+        }
+    }
 `;
 
 export const ImagemEu = styled.img`
-    width: 20%;
+    width: 60%;
     height: auto;
     border-radius: 50%;
+    
 `;
 
 export const Blob = styled.img`
-    width: 30%;
+    width: 65%;
     height: auto;
-    position: absolute;
     z-index: -1;
-    left: 8em;
-    top: -1em;
+    position: absolute;
+    top: 0em;
+    left: 6em;
+    @media (max-width: 1300px){
+        left: 5em;
+    }
+    @media (max-width: 600px){
+        left: 4em;
+    }
+`;
+
+export const LinkFotografia = styled.a`
+    color: #14ffec;
+    cursor: pointer;
+`;
+
+export const DivIcones = styled.div`
+    display: flex;
+    margin-top: 2em;
+    align-items: flex-end;
+    a{
+        background: transparent;
+        color: #14ffec;
+        border:0;
+        margin-right: 1em;
+        cursor: pointer;
+
+        svg{
+            transition: 0.4s;
+            &:hover{
+                transform: scale(1.4);
+            }
+        }
+        
+    }
+
+    p{
+        color: #14ffec;
+    }
 `;
